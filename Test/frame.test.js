b7e88e103294e5458b5a9ae5652071da38eea4a1
mikilukasik <szarhazi@gmail.com>
2016-01-25 20:39:13 +0000

    .

---
+++ Test/frame.test.js
@@ -0,0 +1,14 @@
+
+var assert = require('assert');
+
+var frame = require('../app.frame.js');
+
+
+describe('test ai /',function(){
+    describe('test single thread calc /',function(){
+       
+        it('generate inited table',function(){
+          
+        
+    })
+})
